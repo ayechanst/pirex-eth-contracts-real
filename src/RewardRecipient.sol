f628061042c36d97233bb06f110fbd0787749513
ayechan <ayechan.sant@gmail.com>
2024-09-26 08:54:42 -0700

    RewardRecipient.sol

--- src/RewardRecipient.sol
+++ src/RewardRecipient.sol
@@ -7,51 +7,51 @@ import {IOracleAdapter} from "./interfaces/IOracleAdapter.sol";
 import {IPirexEth} from "./interfaces/IPirexEth.sol";
 import {Errors} from "./libraries/Errors.sol";
 
-/**
- * @title RewardRecipient
- * @notice Manages rewards for validators and handles associated functionalities.
- * @dev Inherits from AccessControlDefaultAdminRules to control access to critical functions.
- * @author redactedcartel.finance
+/*
+  @title RewardRecipient
+  @notice Manages rewards for validators and handles associated functionalities.
+  @dev Inherits from AccessControlDefaultAdminRules to control access to critical functions.
+  @author redactedcartel.finance
  */
 contract RewardRecipient is AccessControlDefaultAdminRules {
-    /**
-     * @notice The role assigned to external keepers responsible for specific protocol functions.
-     * @dev This constant represents the keccak256 hash of the string "KEEPER_ROLE".
+    /*
+      @notice The role assigned to external keepers responsible for specific protocol functions.
+      @dev This constant represents the keccak256 hash of the string "KEEPER_ROLE".
      */
     bytes32 private constant KEEPER_ROLE = keccak256("KEEPER_ROLE");
 
-    /**
-     * @notice The role assigned to governance entities responsible for managing protocol parameters.
-     * @dev This constant represents the keccak256 hash of the string "GOVERNANCE_ROLE".
+    /*
+      @notice The role assigned to governance entities responsible for managing protocol parameters.
+      @dev This constant represents the keccak256 hash of the string "GOVERNANCE_ROLE".
      */
     bytes32 private constant GOVERNANCE_ROLE = keccak256("GOVERNANCE_ROLE");
 
     // Pirex contracts
-    /**
-     * @notice The IPirexEth interface for interacting with the PirexEth contract.
-     * @dev This interface defines the methods available for communication with the PirexEth contract.
+    /*
+      @notice The IPirexEth interface for interacting with the PirexEth contract.
+      @dev This interface defines the methods available for communication with the PirexEth contract.
      */
     IPirexEth public pirexEth;
 
-    /**
-     * @notice The OracleAdapter contract responsible for interfacing with the oracle for protocol data.
-     * @dev This contract provides receives update when validator is dissolved.
+    /*
+      @notice The OracleAdapter contract responsible for interfacing with the oracle for protocol data.
+      @dev This contract provides receives update when validator is dissolved.
      */
     IOracleAdapter public oracleAdapter;
 
     // Events
-    /**
-     * @notice Emitted when a contract address is set.
-     * @dev Signals changes to contract addresses, indicating updates to PirexEth or OracleAdapter.
-     * @param c               DataTypes.Contract Enum indicating the contract type.
-     * @param contractAddress address            The new address of the contract.
+    /*
+      @notice Emitted when a contract address is set.
+      @dev Signals changes to contract addresses, indicating updates to PirexEth or OracleAdapter.
+      @param c               DataTypes.Contract Enum indicating the contract type.
+      @param contractAddress address            The new address of the contract.
      */
     event SetContract(DataTypes.Contract indexed c, address contractAddress);
 
     // Modifiers
-    /**
-     * @notice Modifier to restrict access to the function only to the Oracle Adapter.
-     * @dev Reverts with an error if the caller is not the Oracle Adapter.
+    /*
+      @notice Modifier to restrict access to the function only to the Oracle Adapter.
+      @dev Reverts with an error if the caller is not the Oracle Adapter.
      */
     modifier onlyOracleAdapter() {
         if (msg.sender != address(oracleAdapter))
@@ -59,22 +59,22 @@ contract RewardRecipient is AccessControlDefaultAdminRules {
         _;
     }
 
-    /**
-     * @notice Constructor to set the admin and initial delay for access control transfer.
-     * @dev Initializes the contract with the specified admin address and initial delay for access control transfer.
-     * @param _admin        address Admin address.
-     * @param _initialDelay uint48  Initial delay required for the acceptance of an access control transfer.
+    /*
+      @notice Constructor to set the admin and initial delay for access control transfer.
+      @dev Initializes the contract with the specified admin address and initial delay for access control transfer.
+      @param _admin        address Admin address.
+      @param _initialDelay uint48  Initial delay required for the acceptance of an access control transfer.
      */
     constructor(
         address _admin,
         uint48 _initialDelay
     ) AccessControlDefaultAdminRules(_initialDelay, _admin) {}
 
-    /**
-     * @notice Set a contract address.
-     * @dev Allows a contract address to be set by the governance role.
-     * @param _contract       enum    Contract.
-     * @param contractAddress address Contract address.
+    /*
+      @notice Set a contract address.
+      @dev Allows a contract address to be set by the governance role.
+      @param _contract       enum    Contract.
+      @param contractAddress address Contract address.
      */
     function setContract(
         DataTypes.Contract _contract,
@@ -93,11 +93,11 @@ contract RewardRecipient is AccessControlDefaultAdminRules {
         }
     }
 
-    /**
-     * @notice Dissolve a validator.
-     * @dev Allows the dissolution of a validator by the OracleAdapter.
-     * @param _pubKey bytes   Key of the validator.
-     * @param _amount uint256 ETH amount.
+    /*
+      @notice Dissolve a validator.
+      @dev Allows the dissolution of a validator by the OracleAdapter.
+      @param _pubKey bytes   Key of the validator.
+      @param _amount uint256 ETH amount.
      */
     function dissolveValidator(
         bytes calldata _pubKey,
@@ -106,15 +106,15 @@ contract RewardRecipient is AccessControlDefaultAdminRules {
         pirexEth.dissolveValidator{value: _amount}(_pubKey);
     }
 
-    /**
-     * @notice Slash a validator.
-     * @dev Allows the slashing of a validator by a Keeper, potentially using a buffer for penalty compensation.
-     * @param _pubKey         bytes                     Key of the validator.
-     * @param _removeIndex    uint256                   Validator public key index.
-     * @param _amount         uint256                   ETH amount released from Beacon chain.
-     * @param _unordered      bool                      Removed in a gas-efficient way or not.
-     * @param _useBuffer      bool                      Whether to use a buffer to compensate for the penalty.
-     * @param _burnerAccounts DataTypes.BurnerAccount[] Burner accounts.
+    /*
+      @notice Slash a validator.
+      @dev Allows the slashing of a validator by a Keeper, potentially using a buffer for penalty compensation.
+      @param _pubKey         bytes                     Key of the validator.
+      @param _removeIndex    uint256                   Validator public key index.
+      @param _amount         uint256                   ETH amount released from Beacon chain.
+      @param _unordered      bool                      Removed in a gas-efficient way or not.
+      @param _useBuffer      bool                      Whether to use a buffer to compensate for the penalty.
+      @param _burnerAccounts DataTypes.BurnerAccount[] Burner accounts.
      */
     function slashValidator(
         bytes calldata _pubKey,
@@ -137,11 +137,11 @@ contract RewardRecipient is AccessControlDefaultAdminRules {
         );
     }
 
-    /**
-     * @notice Harvest and mint staking rewards.
-     * @dev Allows a Keeper to trigger the harvest of staking rewards and mint the corresponding amount of ETH.
-     * @param _amount   uint256 Amount of ETH to be harvested.
-     * @param _endBlock uint256 Block until which ETH rewards are computed.
+    /*
+      @notice Harvest and mint staking rewards.
+      @dev Allows a Keeper to trigger the harvest of staking rewards and mint the corresponding amount of ETH.
+      @param _amount   uint256 Amount of ETH to be harvested.
+      @param _endBlock uint256 Block until which ETH rewards are computed.
      */
     function harvest(
         uint256 _amount,
@@ -150,9 +150,9 @@ contract RewardRecipient is AccessControlDefaultAdminRules {
         pirexEth.harvest{value: _amount}(_endBlock);
     }
 
-    /**
-     * @notice Receive MEV rewards.
-     * @dev Allows the contract to receive MEV rewards in the form of ETH.
+    /*
+      @notice Receive MEV rewards.
+      @dev Allows the contract to receive MEV rewards in the form of ETH.
      */
     receive() external payable {}
 }
